ffbc2a2d175681d6875374896a3cb79e26439f84
Ethiopian-boy <mullerfromethiopia@gmail.com>
2022-08-25 06:02:26 +0000

    C - Binary trees

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - a function that finds the sibling of a node
+ * @node: a pointer to the node to find the sibling
+ * Return: a pointer to the sibling node, NULL if node has no sibiling
+ * or node is NULL or the parent is NULL
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	binary_tree_t *new;
+
+	new = node->parent;
+
+	if (node == new->left)
+	{
+		return (new->right);
+	}
+	return (new->left);
+}
