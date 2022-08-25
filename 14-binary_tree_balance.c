d86c59ffed9eb97a01ea7b685dbb6284f41a655b
Ethiopian-boy <mullerfromethiopia@gmail.com>
2022-08-25 10:11:59 +0000

    balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,9 +9,10 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	size_t h_L = 0, h_R = 0, bf = 0;
+
 	if (!tree)
 		return (0);
-	size_t h_L = 0, h_R = 0, bf = 0;
 
 	h_L = binary_tree_height(tree->left);
 	h_R = binary_tree_height(tree->right);
